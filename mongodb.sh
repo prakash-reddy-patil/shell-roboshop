a343284d3d0d6a0aa60a8dbd7b8b450f10068b6b
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-17 16:13:24 +0530

    comitted

--- mongodb.sh
+++ mongodb.sh
@@ -41,3 +41,9 @@ VALIDATE $? "Enable MongoDB"
 
 systemctl start mongod   &>>$LOG_FILE
 VALIDATE $? "Start MongoDB"
+
+sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connection to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
