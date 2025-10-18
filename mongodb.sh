3c1454ab536bd08ccdde6237e26face147ff8e0b
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-18 18:06:42 +0530

    files are comitted

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
