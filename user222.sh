779b70b6ef8260447da448417b7f0f19559f83e9
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-11-04 22:43:34 +0530

    session-26

---
+++ user222.sh
@@ -0,0 +1,20 @@
+- name: "prakashreddy"
+  dob:26 march 1994
+  age:26
+  addresses:
+    # object
+  - doorno:1-32
+     street:"gandi nagr"
+     city:hyd
+     pin:1234
+     type:current
+
+  - doorno:1-32
+     street:"gandi nagr"
+     city:hyd
+     pin:1234
+     type:permanent 
+  skills
+  - java
+  - devops
+  - cloud
